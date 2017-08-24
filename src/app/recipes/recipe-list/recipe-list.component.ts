857ee707b58d8d02cdd37e40b92d1c93ad1bbd53
Jim Block <blockdx@gmail.com>
2017-08-24 15:04:06 +0000

    Add appDropdown directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe("Test Recipe","This is simply a test","https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Pindi-chana-recipe.jpg/800px-Pindi-chana-recipe.jpg"),
-    new Recipe("Test Recipe","This is simply a test","https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Pindi-chana-recipe.jpg/800px-Pindi-chana-recipe.jpg")
+    new Recipe("Test Recipe 2","This is simply a test... again!","https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Pindi-chana-recipe.jpg/800px-Pindi-chana-recipe.jpg")
   ];
   
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
